f4946c28e0e4036ef4662c6f12132d693d0ae5b8
Tricky1975 <facebook@tbbs.nl>
2017-12-29 10:19:36 +0100

    Directory creation and platform asking

--- src/meta.go
+++ src/meta.go
@@ -26,7 +26,9 @@ package main
 
 
 import (
+	"os"
 	"trickyunits/mkl"
+	"trickyunits/qff"
 	"trickyunits/qstr"
 )
 
@@ -47,4 +49,22 @@ func askmeta(){
 func asksys() { // strictly speaking not meta, but handier to have it here.
 	pask("Release","Release dir:","")
 	pask("Test","Test dir:","")
+	if !qff.Exists(prjgini.C("Release."+platform)) {
+		if yes("","Release dir does not yet exist. Create it? ") {
+			os.MkdirAll(prjgini.C("Release."+platform),0777)
+		} else {
+			os.Exit(0)
+		}
+	}
+	if !qff.Exists(prjgini.C("Test."+platform)) {
+		if yes("","Test dir does not yet exist. Create it? ") {
+			os.MkdirAll(prjgini.C("Test."+platform),0777)
+		} else {
+			os.Exit(0)
+		}
+	}
+	yes("Mac64","Do you want to create a mac build")
+	yes("Win32","Do you want to create a windows 32bit build")
+	yes("Win64","Do you want to create a windows 64bit build")
+	yes("Linux","I cannot build for Linux yet, but do you want to create a file in a separate Linux folder for further packaging")
 }
