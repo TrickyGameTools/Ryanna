3b7165dcb4519505a3fd9184c147f257065d92a9
Tricky1975 <facebook@tbbs.nl>
2017-12-30 15:31:55 +0100

    luago updated

--- src/luascript.go
+++ src/luascript.go
@@ -6,34 +6,29 @@ import "trickyunits/mkl"
 
 func init(){
 	script["main"] = `--[[
-	Ryanna - Script
-	
-	
-	
-	
-	(c) Jeroen P. Broks, 2017, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 17.12.30
+  main.lua
+  
+  version: 17.12.30
+  Copyright (C) 2017 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
 ]]
 -- basis script
 
 --[[
-mkl.version("Ryanna - Builder for jcr based love projects - basis.lua","17.12.30")
-mkl.lic    ("Ryanna - Builder for jcr based love projects - basis.lua","GNU General Public License 3")
+mkl.version("Ryanna - Builder for jcr based love projects - main.lua","17.12.30")
+mkl.lic    ("Ryanna - Builder for jcr based love projects - main.lua","ZLib License")
 ]]
 
 
@@ -50,7 +45,7 @@ Ryanna = {
 
 -- include use.Lua and jcr6.lua which now have not made their official entrace, so I gotta call them manually
 function load_primary_dependencies()
-	for _,dep in {"jcr6.lua","use.lua"}
+	for _,dep in {"jcr6.lua","use.lua","preprocess.lua"}
 		chunk, errormsg = love.filesystem.load( name )
 		assert(chunk,errormsg)
 		chunk()
@@ -212,6 +207,13 @@ for i=1,len(haystack) do
 return ret    
 end
 
+function prefixed(str,prefix)
+	return left(str,#prefix)==prefixed
+end
+
+function suffixed(str,prefix)
+	return right(str,#prefix)==prefixed
+end
 
 
 
@@ -312,29 +314,263 @@ assert(RYANNA_MAIN_SCRIPT and RYANNA_MAIN_SCRIPT~="","There has no script been a
 Use(RYANNA_MAIN_SCRIPT)
 `
 
+	script["use"] = `--[[
+  use.lua
+  Ryanna - Script
+  version: 17.12.30
+  Copyright (C) 2017 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+-- Importer
+
+--[[
+mkl.version("Ryanna - Builder for jcr based love projects - use.lua","17.12.30")
+mkl.lic    ("Ryanna - Builder for jcr based love projects - use.lua","ZLib License")
+]]
+
+
+
+-- Now in stead of using this function, you can also make a call to the 
+-- preprocessor. Especially for external libraries this is recommended 
+-- As that will cause cause Ryanna to import them into the .love file
+-- automatically (that happens for all calls made to libs/
+-- .lua or .rel are allowed (to make sure stuff is found correctly,
+-- but is not required)
+function Use(imp,noreturn)
+	-- single file
+	wimp = string.Upper(imp)
+	if right(wimp,4)==".LUA" then
+		ret = PreProcess(imp)
+		if noreturn then return nil else return ret end
+	end
+	if JCR_Exists(imp..".lua") then return Use(imp..".lua",noreturn) end
+	-- Is this a Ryanna Expanded Library then?
+	if right(wimp,4)~=".REL" then
+		assert(JCR_HasDir(imp..".rel"),"Nothing appears to match the use request: "..imp)
+		return Use(imp..".rel",noreturn)
+	end
+	-- Import all the data
+	pret = {} -- pre return
+	for ename,entry in spairs(jcr.Entries) do
+		if prefixed(ename,wimp+"/") and suffixed(ename,".LUA") then
+			name = right(entry.entry,#entry.entry-(#imp+1))
+			name = left(entry.entry,#entry.entry-4)
+			pret[name] = PreProcess(entry.entry)
+		end
+	-- Count it all
+	cnt = 0
+	for k,v in pairs(pret) do cnt = cnt + 1   lk = k end
+	assert(cnt>0,"Ryanna Expanded Library is empty: "+imp)
+	if noreturn then return end
+	if cnt==1 then
+		return pret[lk]
+	end
+	-- process the returning result
+	ret = {}
+	ret.me = ret
+	for k,v in pairs(pret) do
+		if type(v)=="table" then
+			if v.nomerge then
+				if v.me then print("WARNING! 'me' field set in module part.") else v.me = ret end
+				ret[k] = v
+			else 
+				for k2,v2 in pairs(v) do
+					if ret[k2] then print("WARNING! Duplicate identifier '"+k2+"' found!") end
+					ret[k2] = v2
+				end
+			end
+		else
+			ret[k] = v
+		end
+	end
+	return ret
+end
+
+
+-- If you really need to destroy a module this is the wisist way to do so
+-- before you set it to 'nil'. Otherwise stuff will not be picked up by 
+-- Lua's garbage collector correctly causing "big boom" in the process.
+-- In other words, memory leaks.
+function libdestroy(lib)
+	if type(lib)~='table' then return end -- Only needed for tables
+	if not lib.me then return end
+	lib.me = nil
+	local fld = {}
+	for key,v in pairs(lib) then libdestroy(v); fld[#fld+1]=key end
+	for i,key in ipairs(fld) do lib[key] = nil end
+end
+`
+
+	script["preprocess"] = `--[[
+  preprocess.lua
+  
+  version: 17.12.30
+  Copyright (C) 2017 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+]]
+
+
+preiftrueerror = false
+
+local defs = {}
+local plat = string.upper(love.system.getOS( ))
+if plat = "OS X" then defs["$MAC"] = true defs["$DARWIN"]=true defs["$OSX"]=true end
+if plat = "WINDOWS" then defs["$WIN"] = true defs["$WINDOWS"] = true defs["$WINDHOOS"] = true defs["$MICROSCHOFT"] = true end
+if plat = "LINUX" then defs["$LINUX"] = true defs["$INSTABIEL"] = true end
+if plat = "ANDROID" then defs["$ANDROID"] = true defs["$MOBILE"] = true end
+if plat = "IOS" then defs["$IOS"] = true defs["$MOBILE"]=true end
+
+local prid = {
+	["IF"] = function(sl,h,m,n,ld)
+		assert(not h,"Duplicate $IF in line: "..n)
+		assert(#sl>2,"$IF expects statements in line: "..n)
+		local pline = "local chk = {}\n"
+		-- global defs
+		for k,v in pairs(defs) do pline = pline .. "chk['"..k.."'] = true\n" end -- This makes sure we got all locals in our little function.
+		-- localdefs defs
+		for k,v in pairs(ld) do pline = pline .. "chk['"..k.."'] = true\n" end -- This makes sure we got all locals in our little function.
+		pline = "\n\nreturn "
+		for i=3,#sl do
+			w=string.upper(sl[i])
+			if w=="OR" or w=="AND" then pline = pline .. string.lower(w) .. " "
+			elseif prefixed(w,1)=="!" then pline = pline .. " (not chk['"+w+"']) "
+			else   pline = pline .. "(chk['"+w+"') " end
+		end
+		ok,chkf = pcall(load(pline,"$IF"))
+		if not ok then
+			print("$IF went wrong in line: "..n)
+			print("-- GENERATED CODE --"
+			print(pline)
+			print("-- END CODE --")
+			print("error: "..chkf)
+			error("Invalid $IF call in line: "..n)
+		end
+		local mute = not chkf()
+		return true,mute
+	end,
+	
+	["ELSE"] = function(sl,h,m,n,ld)
+		assert(h,"$ELSE without $IF in "..n)
+		return true,not m
+	end,
+	
+	["FI"] = function(sl,h,m,n,ld)
+		assert(h,"$FI without $IF in "..n)
+		return false,false
+	end,
+	
+	["DEFINE"] = function(sl,h,m,n,ld)
+		if h and m then return h,m end
+		assert(#sl>2,"$DEFINE expects options in line: "..n)
+		d = string.upper(sl[3])
+		if prefixed(d,"#") then
+			ld[d]=true
+		else
+			defs[d]=true
+		end
+		return h,m
+	end,
+
+	["UNDEF"] = function(sl,h,m,n,ld)
+		if h and m then return h,m end
+		assert(#sl>2,"$UNDEF expects options in line: "..n)
+		d = string.upper(sl[3])
+		if prefixed(d,"#") then
+			ld[d]=nil
+		else
+			defs[d]=nil
+		end
+		return h,m
+	end,
+	
+	["USE"] = function(sl,h,m,n,ld)
+		if h and m then return h,m end
+		assert(#sl>2,"$USE expects libraries in line: "..n)		
+		local bfs = mysplit(sl[3],"/")
+		local bf = bfs[#bfs]
+		local exs = mysplit(bf,".")
+		local id = exs[1]
+		local x = string.upper(sl[4] or "")
+		local pre = id .. " = "..id.." or "
+		if x == "NOAS" then
+			pre = ""
+		end
+		if x == "AS" then
+			assert(sl[5],"AS without identifier")
+			asid = sl[5]
+			if prefixed(asid,"#") then asid = right(asid,#asid-1) pre = "local " else pre = " " end
+			pre = pre .. asid .. " = " ..asid.. " or "
+		end
+		return h,m,pre .. " Use('"..sl[3].."') "
+
+}
+
+function PreProcess(file)
+	local d = JCR_Lines(file)
+	local haveif
+	local muteif
+	local ret = ""
+	local localdefs = {}
+	for lnum,line in ipairs(d) do
+		sline = mysplit(trim(line))
+		if prefixed(trim(line),"-- $") then
+			sline[2]=strings.upper(sline[2])
+			cmd = strings.upper(sline[2])
+			assert(prid[cmd],"UNKNOWN PRE-PROCESSOR DIRECTIVE in line "..lnum.." ("..cmd..")")
+			haveif,muteif,rl = prid[cmd](sline,haveif,muteif,lnum,localdefs)
+			ret = ret .. (rl or line) .. "\n"
+		elseif haveif and muteif then
+			ret = ret .. "-- muted: "..line.."\n" -- This takes extra ram, but this way the line numbers in traceback routines remain.
+		else
+			ret = ret .. line .."\n"
+		end
+	end
+	return ret
+end
+`
+
 	script["jcr6"] = `--[[
-	Ryanna - Script
-	
-	
-	
-	
-	(c) Jeroen P. Broks, 2017, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 17.12.30
+  jcr6.lua
+  Ryanna - Script
+  version: 17.12.30
+  Copyright (C) 2017 Jeroen P. Broks
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
 ]]
 local ldir = love.filesystem.getSourceBaseDirectory()
 
@@ -375,7 +611,7 @@ function LOVE_Dir(skipwork) -- if set to true it will skip the directories swap 
 	return ret
 end
 
-function JCR_B(j,nameentry)
+function JCR_B(j,nameentry,lines)
 	local mj
 	if not nameentry then
 		entry = string.upper(j)
@@ -400,10 +636,60 @@ function JCR_B(j,nameentry)
 	sl = bt:readlines()
 	assert(sl[1]=="OK",sl[2])
 	bt:close()
-	s = ""
-	for i=2,#sl do s = s .. sl[i] .. "\n" end
+	if lines then
+		s = {}
+		for i=2,#sl do s[#s+1] = sl[i] end
+	else
+		s = ""
+		for i=2,#sl do s = s .. sl[i] .. "\n" end
+	end
 	return s
 end
+
+function JCR_Lines(j,nameentry)
+	return JCR_B(j,nameentry,true)
+end
+
+function JCR_Exists(j,nameentry)
+	local mj
+	if not nameentry then
+		entry = string.upper(j)
+		mj = jcr
+		assert ( mj , "JCR not set!" )
+	else
+		entry = string.upper(nameentry)
+		if type(mj)=='table' then
+			mj = j
+		else 
+			mj = JCR_Dir(j)
+		end
+	end
+	e = string.upper(entry)
+	edata = mj.entries[e]
+	return edata~=nil
+end
+
+function JCR_HasDir(j,namedir)
+	local mj
+	if not namedir then
+		dir= string.upper(j)
+		mj = jcr
+		assert ( mj , "JCR not set!" )
+	else
+		dir= string.upper(namedir)
+		if type(mj)=='table' then
+			mj = j
+		else 
+			mj = JCR_Dir(j)
+		end
+	end
+	if not suffixed(dir) then dir = dir .. "/" end
+	for ent,_ in pairs(mj) do
+		if prefixed(ent,dir) then return true end
+	end
+	return false
+end
+
 
 
 function BaseDir() -- Basically only called by Ryanna and loaded based on Ryanna's findings.
@@ -420,22 +706,26 @@ function BaseDir() -- Basically only called by Ryanna and loaded based on Ryanna
 	end
 	return ret
 end
-BaseDir()
+jcr = BaseDir()
 
 
 --[[
 mkl.version("Ryanna - Builder for jcr based love projects - jcr6.lua","17.12.30")
-mkl.lic    ("Ryanna - Builder for jcr based love projects - jcr6.lua","GNU General Public License 3")
+mkl.lic    ("Ryanna - Builder for jcr based love projects - jcr6.lua","ZLib License")
 ]]
 `
 
-	/* Lua */ mkl.Version("Ryanna - Builder for jcr based love projects - basis.lua","17.12.30")
+	/* Lua */ mkl.Version("Ryanna - Builder for jcr based love projects - main.lua","17.12.30")
 
-	/* Lua */ mkl.Lic    ("Ryanna - Builder for jcr based love projects - basis.lua","GNU General Public License 3")
+	/* Lua */ mkl.Lic    ("Ryanna - Builder for jcr based love projects - main.lua","ZLib License")
+
+	/* Lua */ mkl.Version("Ryanna - Builder for jcr based love projects - use.lua","17.12.30")
+
+	/* Lua */ mkl.Lic    ("Ryanna - Builder for jcr based love projects - use.lua","ZLib License")
 
 	/* Lua */ mkl.Version("Ryanna - Builder for jcr based love projects - jcr6.lua","17.12.30")
 
-	/* Lua */ mkl.Lic    ("Ryanna - Builder for jcr based love projects - jcr6.lua","GNU General Public License 3")
+	/* Lua */ mkl.Lic    ("Ryanna - Builder for jcr based love projects - jcr6.lua","ZLib License")
 
 
 
